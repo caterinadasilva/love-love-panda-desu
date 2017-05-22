84855f20caad1e0ffc226302e44e489fd9894e19
Syrimande <caterinadasilva@gmail.com>
2017-05-21 21:19:49 -0400

    Funcionalidad de galería de imágenes agregada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+function imgs() {
+	var container = document.getElementById('pic-container');
+	var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+	
+	pictures.forEach(function(img) {
+		var foto = document.createElement('img');
+		foto.src = img;
+		container.appendChild(foto);
+	})
+}
+
